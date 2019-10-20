419eae78bf2774f618dfa081b505fcf4fc1e83e4
kiyong <starmonhong@naver.com>
2019-10-21 04:17:02 +0900

    Updates

---
+++ precache-manifest.073a3587c9fb92edcf4d278ee80226d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5df100d13e1977ad0d616c1312574b",
+    "url": "/moive_app_2019/index.html"
+  },
+  {
+    "revision": "4da7830aabb1da9b37b9",
+    "url": "/moive_app_2019/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "0839e41b5365117736fe",
+    "url": "/moive_app_2019/static/js/2.b733c6f9.chunk.js"
+  },
+  {
+    "revision": "4da7830aabb1da9b37b9",
+    "url": "/moive_app_2019/static/js/main.90651cd4.chunk.js"
+  },
+  {
+    "revision": "e04ffb6dd41fddcb3be4",
+    "url": "/moive_app_2019/static/js/runtime-main.6096179b.js"
+  }
+]);
